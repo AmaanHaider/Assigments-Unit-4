18ba1f9d2edfa538b30285249338c2405e6725da
AmaanHaider <97442978+AmaanHaider@users.noreply.github.com>
2022-03-10 01:18:05 +0530

    Updated

--- Eventloops/index.js
+++ Eventloops/index.js
@@ -1,14 +1,19 @@
+
+
+
 const express= require("express");
+
+
+
+
 const app= express();
-
-
 
 app.get("/home", function(req, res){
     res.send("hello")
 });
 
 app.get("/books", function(req, res){
-    res.send({book1:"I am first bookk", book2:"I am second book",book3:"I am third book", book4:"I am fourth book"})
+    res.send({book1:"I am first book", book2:"I am second book",book3:"I am third book", book4:"I am fourth book"})
 });
 app.listen(5000,()=>{
     console.log("Listening to path 5000")
