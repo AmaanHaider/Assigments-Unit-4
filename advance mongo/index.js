e9839f4074c89f757f713f39d599623d56690d3d
AmaanHaider <97442978+AmaanHaider@users.noreply.github.com>
2022-03-16 01:29:57 +0530

    update

--- advance mongo/index.js
+++ advance mongo/index.js
@@ -9,11 +9,11 @@ const connect =()=>{
     return mongoose.connect("mongodb+srv://haider:haider88888@cluster0.apitv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 }
 
-const schema =mongoose.schema({
+const schema =mongoose.Schema({
 
     id:String,
     movie_name:String,
-    movie_name:String,
+    movie_genre:String,
     production_year:String,
     budget:String,
 });
@@ -21,14 +21,16 @@ const schema =mongoose.schema({
 const user=mongoose.model("mockdata",schema);
 
 app.get("/mockdata", async(req,res)=>{
-
-
+    // .movie.find({"name":"Gladiator"});
+   await user.find({}).lean().exec();
+    console.log(user)
+    return res.send(user)
 })
 
 app.listen(9000,async()=>{
     try {
         await connect();
-        console.log("moj 9000")
+        console.log("Listening to 9000")
         
     } catch (error) {
         console.log("error")
